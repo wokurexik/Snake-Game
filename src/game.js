be706faf1dea5312f36a7f0b1f3af321d4033ae3
Wokurexik <slama.vit7@gmail.com>
2024-02-22 21:06:13 +0100

    media query, Best score

--- src/game.js
+++ src/game.js
@@ -7,26 +7,46 @@ let gameOver = false
 const gameBoard = document.getElementById('game-board')
 const backgroundColor = document.getElementById('backgroundColor')
 const score = document.getElementById("score")
+const bestScore = document.getElementById("bestScore")
 
 backgroundColor.addEventListener('input', function(){
     draw(document.getElementById('game-board'));
 })
 
+const topScore = [];
+let currentBestScore = localStorage.getItem("bestScore") || 0;
+
+// Display the initial best score when the page loads
+bestScore.textContent = "Best Score: " + currentBestScore;
+
 function main(currentTime) {
     
     if (gameOver) {
 
-        if(confirm('You lose!' + " " + 'Tvé score je: ' + score.textContent )){
-            location.reload()
-        }
-        return
+        const currentScore = parseInt(score.textContent);
+        topScore.push(currentScore);
+        topScore.sort((a, b) => b - a);
+        const topScoresToShow = topScore.slice(0, 1);
 
+        // Update the best score only if it's a new best score
+        if (topScoresToShow[0] > currentBestScore) {
+            currentBestScore = topScoresToShow[0];
+            bestScore.textContent = "Best Score: " + currentBestScore;
+
+            // Store the best score in localStorage
+            localStorage.setItem("bestScore", currentBestScore);
+        }
+
+        if(confirm('You lose!' + " " + 'Your score is: ' + score.textContent )){
+            location.reload();
+        }
+
+        return;
     }
     
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-
 
     lastRenderTime = currentTime
 
