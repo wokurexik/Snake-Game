fb007b57c44f1aea0fee0311f0b15750d0b9318f
Wokurexik <slama.vit7@gmail.com>
2024-03-01 11:19:40 +0100

    new features

--- src/input.js
+++ src/input.js
@@ -1,34 +1,36 @@
-let inputDirection = { x: 0, y: 0}
-let lastInputDirection = { x: 0, y: 0}
-let isPaused = false
+let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
+let isPaused = false;
 
 window.addEventListener('keydown', e => {
     switch (e.key.toLowerCase()) {
-    case 'w':
-        if (lastInputDirection.y !== 0) break
-        inputDirection = { x: 0, y: -1 };
-        break
+        case 'w':
+        case 'arrowup':
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: -1 };
+            break;
 
-    case 's':
-        if (lastInputDirection.y !== 0) break
-        inputDirection = { x: 0, y: 1 };
-        break
+        case 's':
+        case 'arrowdown':
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: 1 };
+            break;
 
-    case 'a':
-        if (lastInputDirection.x !== 0) break
-        inputDirection = { x: -1, y: 0 };
-        break
+        case 'a':
+        case 'arrowleft':
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: -1, y: 0 };
+            break;
 
-    case 'd':
-        if (lastInputDirection.x !== 0) break
-        inputDirection = { x: 1, y: 0 };
-        break
-
-}
-})
+        case 'd':
+        case 'arrowright':
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: 1, y: 0 };
+            break;
+    }
+});
 
 export function getInputDirection() {
-    lastInputDirection = inputDirection
-    return inputDirection
+    lastInputDirection = inputDirection;
+    return inputDirection;
 }
-
